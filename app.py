a8ff44e6273715c544563262ca7be82182efc652
seanlewis <stkl@STKL.fritz.box>
2015-07-17 11:22:13 +1200

    connected to db and showing info

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ app = Flask(__name__)
 # Connect to MongoDB at localhost:27017 
 client = MongoClient()
 
+# Connect to the aerodb database
 db = client.aerodb
 
 @app.route("/")
 def test_connection():
+    items = db.aerodromes.find()
+
+    return render_template('index.html',
+        items=items)
+
+@app.route("/<icao>")
+def test_connection(icao):
     items = db.aerodromes.find()
 
     return render_template('index.html',
